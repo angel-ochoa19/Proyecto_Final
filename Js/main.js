45168d0f51682ee00c4b6eae3df83779520f8a28
Angel <aochoac3@miumg.edu.gt>
2023-05-30 17:25:38 -0800

    agregue contenido nuevo

--- Js/main.js
+++ Js/main.js
@@ -1,19 +1,18 @@
-let miBoton = document.getElementById("elBoton");
-let miParrafo = document.getElementById("elParrafo");
+function aplicarEstilo(numero){
+    var titulo = document.getElementById('titulo');
+    var parrafo = document.getElementById('parrafo');
+    var body = document.body;
 
-// dblclick
-// mouseover
-// keydown
-// mouseout
+    titulo.classList = "estilo" + numero;
+    parrafo.classList = "estilo" + numero;
+    body.classList = "estilo" + numero;
+}
+function resetear(numero){
+    var titulo = document.getElementById('titulo');
+    var parrafo = document.getElementById('parrafo');
+    var body = document.body;
 
-miParrafo.addEventListener("mouseover", () => {
-
-    miParrafo.classList.toggle("parrafo-azul");
-
-})
-
-miParrafo.addEventListener("dblclick", () => {
-
-    miParrafo.classList.toggle("parrafo-rojo");
-
-})
+    titulo.classList = "";
+    parrafo.classList = "";
+    body.classList = "";
+}
